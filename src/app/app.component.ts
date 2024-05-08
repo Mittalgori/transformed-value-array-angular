0720d903e0f467a795131a010907ef8320e0b3a7
Mittalgori <mitu796@gmail.com>
2024-05-08 10:07:59 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { map, of } from 'rxjs';
+import { CommonModule } from '@angular/common';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+ 
   title = 'operators-rxjs-demo';
+  transformedValuesArray: number[] = [];
+
+  ngOnInit(): void {
+    const sourceOfValues$= of(1, 2, 3, 4, 5, 6);
+
+    sourceOfValues$.pipe(
+      map(value => value * 2),
+      filter(value => value % 3 === 0)
+    ).subscribe(
+      transformedDataValue => {
+        this.transformedValuesArray.push(transformedDataValue);
+      }
+    );
+  }
 }
